e5bdd3c270b4896a05b6cfa474c2d5d2736b653a
ubinquitous <ubinquitous1@gmail.com>
2025-01-04 00:09:47 +0900

    feat : initialize

---
+++ src/styles/fonts.ts
@@ -0,0 +1,27 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  variable: '--font-pretendard',
+  src: [
+    {
+      path: '../../public/fonts/Pretendard-Regular.subset.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Bold.subset.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
